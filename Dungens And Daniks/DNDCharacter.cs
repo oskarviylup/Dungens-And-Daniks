4df5f0d9d68e8cdcc3fe4b37094870e243efd805
kirdinDaniil <113731320+kirdinDaniil@users.noreply.github.com>
2023-06-11 22:47:58 +0300

    Добавьте файлы проекта.

---
+++ Dungens And Daniks/DNDCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace Dungens_And_Daniks
+{
+    
+    internal class DNDCharacter
+    {   
+        public string NameOfCharacter { get; set; }
+        public int Level { get; set; }
+        public string Class { get; set; }
+        public string Origin { get; set; }
+        public List<Spell> Spells { get; set; }
+        public string Race { get; set; }
+        public string Ideology { get; set; }
+        public string PersonTraits { get; set; }
+        public string Ideals { get; set; }
+        public string Bonds { get; set; }
+        public string Defects { get; set; }
+        public string History { get; set; }
+        public int HealthDice { get; set; }
+        public List<string> Languages { get; set; }
+
+        public DNDCharacter()
+        {
+            Spells = new List<Spell>();
+            Languages = new List<string>();
+            NameOfCharacter = "Unknown";
+            Level = 0;
+            HealthDice = 0;
+            Class = "???";
+            Origin = "???";
+            Race = "???";
+            Ideology = "???";
+            PersonTraits = "???";
+            Ideals = "???";
+            Bonds = "???";
+            Defects = "???";
+            History = "???";
+        }
+        public string GetJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public async void SaveCharacter()
+        {
+            StringBuilder jsonName = new StringBuilder();
+            jsonName.Append(NameOfCharacter);
+            jsonName.Append(".json");
+            using (FileStream fs = new FileStream(jsonName.ToString(), FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<DNDCharacter>(fs, this);
+            }
+        }
+    }
+}
---
+++ Dungens And Daniks/Spell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungens_And_Daniks
+{
+    internal enum TypeOfSpell
+    {
+        Hil = 1,
+        Defence = 2,
+        Attack = 3,
+        Empty = 0
+    }
+    internal enum CastType
+    {
+        AOE = 1,
+        Target = 2,
+        Empty = 0
+    }
+    internal class Spell
+    {
+        public string NameOfSpell { get; set; }
+        public TypeOfSpell Type { get; set; }
+
+        public CastType Cast { get; set; }
+
+        public int Range { get; set; }
+
+        public string Description { get; set; }
+
+        public Spell()
+        {
+            NameOfSpell = "???";
+            Type = TypeOfSpell.Empty;
+            Cast = CastType.Empty;
+            Range = 0;
+            Description = "???";
+        }
+    }
+}
